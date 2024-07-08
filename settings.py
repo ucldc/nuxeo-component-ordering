6dba19c9bcda78cacea250b166a69e17234041f5
Barbara Hui <barbara.hui@ucop.edu>
2024-07-08 15:16:53 -0700

    Add nuxeo db env vars

--- settings.py
+++ settings.py
@@ -11,4 +11,9 @@ def get_auth():
         credentials, os.environ.get("AWS_REGION", "us-west-2"))
 
 OPENSEARCH_ENDPOINT = os.environ.get("OPENSEARCH_ENDPOINT")
-NUXEO = os.environ.get("NUXEO_TOKEN")
+NUXEO = os.environ.get("NUXEO_API_TOKEN")
+
+DB_NAME = os.environ.get("NUXEO_DB_NAME")
+DB_HOST = os.environ.get("NUXEO_DB_HOST")
+DB_USER = os.environ.get("NUXEO_DB_USER")
+DB_PASS = os.environ.get("NUXEO_DB_PASS")

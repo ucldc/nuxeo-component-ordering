d3e30c040f6a843c8ea8fb1ac4ea1eef754f3054
Barbara Hui <barbara.hui@ucop.edu>
2024-09-16 16:58:53 -0700

    Updates to script

--- complex_objects_no_order.py
+++ complex_objects_no_order.py
@@ -9,6 +9,8 @@ import settings
 def get_complex_obj_no_pos():
     '''
     Find all complex object components (children) where pos is null.
+
+    Returns a list of results in json format.
     '''
     conn = psycopg2.connect(database=settings.NUXEO_DB_NAME,
                         host=settings.NUXEO_DB_HOST,
@@ -36,50 +38,44 @@ def get_complex_obj_no_pos():
     cursor.close()
     conn.close()
 
-    print(f"{results=}")
-    print(f"{type(results)=}")
+    return results[0][0]
 
-    # with open("./output/nuxeo_db_complex_components_null_pos.json", "r") as f:
-    #     complex_obj = f.read()
 
-    # return json.loads(complex_obj)
+def get_nuxeo_data(id):
+    # get full data for object using nuxeo API
+    nuxeo_request_headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "X-NXDocumentProperties": "*",
+            "X-NXRepository": "default",
+            "X-Authentication-Token": settings.NUXEO_API_TOKEN
+        }
 
-# def get_nuxeo_data(id):
-#     # get full data for object using nuxeo API
-#     nuxeo_request_headers = {
-#             "Accept": "application/json",
-#             "Content-Type": "application/json",
-#             "X-NXDocumentProperties": "*",
-#             "X-NXRepository": "default",
-#             "X-Authentication-Token": settings.NUXEO_API_TOKEN
-#         }
+    query = (
+                "SELECT * FROM Documents "
+                f"WHERE ecm:uuid = '{id}' "
+                "AND ecm:isVersion = 0 "
+                "AND ecm:mixinType != 'HiddenInNavigation' "
+                "AND ecm:isTrashed = 0 "
+            )
 
-#     query = (
-#                 "SELECT * FROM Documents "
-#                 f"WHERE ecm:uuid = '{id}' "
-#                 "AND ecm:isVersion = 0 "
-#                 "AND ecm:mixinType != 'HiddenInNavigation' "
-#                 "AND ecm:isTrashed = 0 "
-#             )
+    request = {
+        'url': u'/'.join([settings.NUXEO_API_ENDPOINT, "search/lang/NXQL/execute"]),
+        'headers': nuxeo_request_headers,
+        'params': {
+            'query': query
+        }
+    }
 
-#     request = {
-#         'url': "https://nuxeo.cdlib.org/Nuxeo/site/api/v1/search/lang/NXQL/execute",
-#         'url': f"{settings.NUXEO_API_ENDPOINT}",
-#         'headers': nuxeo_request_headers,
-#         'params': {
-#             'query': query
-#         }
-#     }
+    try:
+        resp = requests.get(**request)
+        resp.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        print(f"unable to fetch components from nuxeo: {request}")
+        raise(e)
 
-#     try:
-#         resp = requests.get(**request)
-#         resp.raise_for_status()
-#     except requests.exceptions.HTTPError as e:
-#         print(f"unable to fetch components from nuxeo: {request}")
-#         raise(e)
-
-#     nuxeo_data = resp.json()
-#     return nuxeo_data
+    nuxeo_data = resp.json()
+    return nuxeo_data
 
 def main():
     '''
@@ -89,6 +85,45 @@ def main():
     '''
     complex_obj_no_pos = get_complex_obj_no_pos()
 
+    print(f"{type(complex_obj_no_pos)=}")
+    print(f"{len(complex_obj_no_pos)=}")
+    print(f"{complex_obj_no_pos[0:10]=}")
+
+    parents = {}
+
+    # for obj in complex_obj_no_pos:
+    #     parentid = obj['parentid']
+    #     if parents.get(parentid):
+    #         parents.get(parentid)['child_count'] += 1
+    #     else:
+    #         parents[parentid] = {"child_count": 1}
+
+    # # we only want a list of parents with more than one child
+    # for id in list(parents.keys()):
+    #     child_count = parents[id]['child_count']
+    #     if child_count <= 1:
+    #         del parents[id]
+
+    # print(f"Querying nuxeo for metadata for {len(parents)} objects")
+    # for id in parents:
+    #     nuxeo_data = get_nuxeo_data(id)
+    #     for entry in nuxeo_data['entries']:
+    #         parents[id]['path'] = entry['path']
+    #         parents[id]['title'] = entry['title']
+    #         parents[id]['type'] = entry['type']
+
+    # # write json file containing parent objects whose children have no `pos`
+    # with open("./output/complex_obj_null_pos.json", "w") as f:
+    #     f.write(json.dumps(parents))
+    # print(f"Wrote ./output/complex_obj_null_pos.json")
+
+    # # write txt file containing parent object paths only
+    # paths = [parents[id]['path'] for id in parents]
+    # paths.sort()
+    # with open("./output/complex_obj_null_pos_paths.txt", "a") as f:
+    #     for path in paths:
+    #         f.write(f"{path}\n")
+    # print(f"Wrote ./output/complex_obj_null_pos_paths.txt")
 
 if __name__ == '__main__':
      main()
